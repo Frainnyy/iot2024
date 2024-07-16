47fe14834ab62f267ab7190ff1e5ac14fc08ea59
PIRAPORN PRASERT <sasayaza@gmail.com>
2024-07-17 00:19:30 +0700

    .

--- 3_fastapi_with_postgresql/app.py
+++ 3_fastapi_with_postgresql/app.py
@@ -49,13 +49,89 @@ async def create_book(book: dict, response: Response, db: Session = Depends(get_
     response.status_code = 201
     return newbook
 
-# @router_v1.patch('/books/{book_id}')
-# async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
-#     pass
+@router_v1.patch('/books/{book_id}')
+async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
+    existing_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not existing_book:
+        return {
+        'message': 'Book not found'
+    }
+    if 'title' in book:
+        existing_book.title = book['title']
+    if 'author' in book:
+        existing_book.author = book['author']
+    if 'year' in book:
+        existing_book.year = book['year']
+    if 'is_published' in book:
+        existing_book.is_published = book['is_published']
+    db.commit()
+    db.refresh(existing_book)
+    return existing_book
 
-# @router_v1.delete('/books/{book_id}')
-# async def delete_book(book_id: int, db: Session = Depends(get_db)):
-#     pass
+@router_v1.delete('/books/{book_id}')
+async def delete_book(book_id: int, db: Session = Depends(get_db)):
+    existing_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not existing_book:
+        return {
+        'message': 'Book not found'
+    }
+    db.delete(existing_book)
+    db.commit()
+    return {"detail": "Book deleted successfully"}
+
+# students
+@router_v1.get('/students')
+async def get_students(db: Session = Depends(get_db)):
+    return db.query(models.Student).all()
+
+@router_v1.get('/students/{student_id}')
+async def get_student(student_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Student).filter(models.Student.id == student_id).first()
+
+@router_v1.post('/students')
+async def create_student(student: dict, response: Response, db: Session = Depends(get_db)):
+    # TODO: Add validation
+    newstudent = models.Student(firstname=student['firstname'], lastname=student['lastname'], std_id=student['std_id'], birth=student['birth'], gender=student['gender'])
+    db.add(newstudent)
+    db.commit()
+    db.refresh(newstudent)
+    response.status_code = 201
+    return newstudent
+
+@router_v1.patch('/students/{student_id}')
+async def update_student(student_id: int, student: dict, db: Session = Depends(get_db)):
+    existing_student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    if not existing_student:
+        return {
+        'message': 'Student not found'
+    }
+
+    if 'firstname' in student:
+        existing_student.firstname = student['firstname']
+    if 'lastname' in student:
+        existing_student.lastname = student['lastname']
+    if 'std_id' in student:
+        existing_student.std_id = student['std_id']
+    if 'birth' in student:
+        existing_student.birth = student['birth']
+    if 'gender' in student:
+        existing_student.gender = student['gender']
+    
+    db.commit()
+    db.refresh(existing_student)
+    return existing_student
+
+@router_v1.delete('/students/{student_id}')
+async def delete_student(student_id: int, db: Session = Depends(get_db)):
+    existing_student = db.query(models.Student).filter(models.Student.id == student_id).first()
+    if not existing_student:
+        return {
+        'message': 'Student not found'
+    }
+    
+    db.delete(existing_student)
+    db.commit()
+    return {"detail": "Student deleted successfully"}
 
 app.include_router(router_v1)
 
