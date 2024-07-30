045dd42d0f901c74ab71e7f024c40062fd94b010
PIRAPORN PRASERT <sasayaza@gmail.com>
2024-07-30 17:45:14 +0700

    .

--- 3_fastapi_with_postgresql/models.py
+++ 3_fastapi_with_postgresql/models.py
@@ -15,11 +15,28 @@ class Book(Base):
     synopsis = Column(String, index=True)
     category = Column(String, index=True)
 
+class Coffee(Base):
+    __tablename__ = 'coffees'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Integer, index=True)
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True, index=True, unique=True, autoincrement=True)
+    coffee_id = Column(Integer, ForeignKey('coffees.id'))
+    quantity = Column(Integer, index=True)
+    total_price = Column(Integer, index=True)
+    notes = Column(String, index=True)
+
 class Student(Base):
     __tablename__ = 'students'
 
-    std_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     firstname = Column(String, index=True)
     lastname = Column(String, index=True)
+    std_id = Column(Integer, index=True)
     birth = Column(String, index=True)
     gender = Column(String, index=True)
